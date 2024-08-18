f9b0c99e3a95fae14ed0ee2efcd19473f1e00bf2
Milani <milani1tetani@gmail.com>
2024-08-18 04:22:40 +0200

    Dice Simulation Game: Added functions for single and double dice rolls

--- dice_simulation.py
+++ dice_simulation.py
@@ -1,19 +1,45 @@
 import random
 
-def roll_dice(): # function that rolls/simulates a random dice number 1 - 6
+def roll_dice(): 
+    """Function that rolls/simulates a random dice number between 1 and 6."""
     return random.randint(1, 6) 
+
+def roll_single_die():
+    """Function for rolling one die"""
+    input("Press Enter to roll a die:\n")
+    roll = roll_dice()
+    print("You have rolled:", roll)
+
+def roll_two_dice():
+    """Function for rolling two dice"""
+    input("Press Enter to roll the dice:\n")
+    roll1 = roll_dice()
+    roll2 = roll_dice()
+    print("You have rolled:", roll1,'and',roll2)    
 
 def main():
     print("\nWelcome to the Dice Simulation Game! \n")
-    while (True):
-        input("Press Enter to play:\n")
-        number = roll_dice()
-        print("You have rolled:", number)
-        reply = input("\nDo you want to play again? \nEnter (yes/no): ")
-        if (reply == "no"):
-            break
+
+    choose = int(input("Would you like to 1 die or 2 dice? \n Enter (1 or 2): "))
+
+    if(choose == 1):
+        while (True):
+            roll_single_die()
+            reply = input("\nDo you want to play again? \nEnter (yes/no): ")
+            if (reply.lower() == "no"):
+                break
+
+    elif(choose == 2):
+        while (True):
+            roll_two_dice()
+            reply = input("\nDo you want to play again? \nEnter (yes/no): ")
+            if (reply.lower() == "no"):
+                break 
+    else:
+        print("Wrong input! Please Enter 1 or 2")
+    
         
-    print("\nThank You for Playing!")
+    print("\nThank You for Playing!")  # End of the game
 
 if __name__=="__main__":
     main()

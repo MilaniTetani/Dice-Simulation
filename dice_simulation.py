9d962ca3f23aa011de7a1e5c0880f10bb3e50d9b
Milani <milani1tetani@gmail.com>
2024-08-21 21:31:12 +0200

    Updated dice simulation game with switch, play, and stop options

--- dice_simulation.py
+++ dice_simulation.py
@@ -6,13 +6,13 @@ def roll_dice():
 
 def roll_single_die():
     """Function for rolling one die"""
-    input("Press Enter to roll a die:\n")
+    input("\nPress Enter to roll:\n")
     roll = roll_dice()
     print("You have rolled:", roll)
 
 def roll_two_dice():
     """Function for rolling two dice"""
-    input("Press Enter to roll the dice:\n")
+    input("\nPress Enter to roll:\n")
     roll1 = roll_dice()
     roll2 = roll_dice()
     print("You have rolled:", roll1,'and',roll2)    
@@ -20,26 +20,42 @@ def roll_two_dice():
 def main():
     print("\nWelcome to the Dice Simulation Game! \n")
 
-    choose = int(input("Would you like to 1 die or 2 dice? \n Enter (1 or 2): "))
+    while (True):
+        choose = int(input("Would you like to roll 1 die or 2 dice? \n Enter (1 or 2): "))
 
-    if(choose == 1):
         while (True):
-            roll_single_die()
-            reply = input("\nDo you want to play again? \nEnter (yes/no): ")
-            if (reply.lower() == "no"):
-                break
+            if(choose == 1):
+                roll_single_die()
+            elif(choose == 2):
+                roll_two_dice()
+            else:
+                print("Wrong input! Please Enter 1 or 2")
+            
+            option = input("\nWould you like to switch between 1 die and 2 dice? (enter switch)\nContinue playing? (enter play)\nOr stop playing? (enter stop)\nI would like to: ")
 
-    elif(choose == 2):
-        while (True):
-            roll_two_dice()
-            reply = input("\nDo you want to play again? \nEnter (yes/no): ")
-            if (reply.lower() == "no"):
-                break 
-    else:
-        print("Wrong input! Please Enter 1 or 2")
-    
+            if option.lower() == "play":
+                continue # Repeats the loop, rolling the current choice of dice
+
+            elif option.lower() == "switch":
+                if (choose == 1):
+                    choose = 2
+                    print("You have changed to 2 dice!\n")
+                    #roll_two_dice()
+                elif (choose == 2):
+                    choose = 1
+                    print("You have change to 1 die!\n")
+                    #roll_single_die()
+            
+            elif option.lower() == "stop":
+                break # Exits the inner loop to end the game
+            
+            else:
+                print("Wrong Input! Please enter 'switch', 'play', or 'stop' ")
+                #continue
         
-    print("\nThank You for Playing!")  # End of the game
+        print("\nThank You for Playing!")  # End of the game
+        break  # Exit the outer loop to completely stop the game
+
 
 if __name__=="__main__":
     main()
